bfd9645c85062b10f885a00779290f8d8c834e4a
Your Name <you@example.com>
2021-10-07 23:45:38 +0100

    projectsection fix

--- src/components/Projectitem.js
+++ src/components/Projectitem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import projectImg from "../assets/images/projectImg.png";
+// import projectImg from "../assets/images/projectImg.png";
 
 const ProjectItemstyles = styled.div`
   .projectitem__img {
